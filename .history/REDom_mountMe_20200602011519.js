a048592e3d8dc964bd0481696e809abf22a3c79a
Christian B <christianbear1221@gmail.com>
2020-06-02 01:15:44 -0700

    ...

---
+++ .history/REDom_mountMe_20200602011519.js
@@ -0,0 +1,44 @@
+// pull imports
+const {el, mount} = redom;
+
+// class that takes in 'el', 'inner', and 'mountPoint'
+// then mounts said 'el' onto 'mountPoint'
+class MountMe{
+  constructor(){
+    this.innerMe=null;
+    this.mountPointMe=null;
+  }  
+
+  
+
+  make(elMe, inner, mountPoint){
+    var tmp = el(elMe, inner);
+    //console.log(mountPoint);
+    if(mountPoint==""||mountPoint==''){
+      mount(document.body, tmp);        // if left "" - then add to document.body
+    }else{
+       mount(document.querySelector(mountPoint), tmp);  
+    }
+
+    this.innerMe = inner;
+    this.mountPointMe = mountPoint;
+    
+  }
+  
+  printMe(){
+    if(!this.mountPointMe){
+      console.log(document.querySelector(this.mountPointMe));
+    }
+ }
+}
+
+
+function printBody(){
+  if(idMe == "" || idMe==''){
+    for(var i=0; i<document.body.children.length; i++){
+      if(document.body.children[i].tagName=="DIV"){
+        console.log(document.body.children[i]);
+      }
+    }
+  }
+}
