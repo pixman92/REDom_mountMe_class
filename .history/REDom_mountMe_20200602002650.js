3643ea1806a4cbfe43e91f8cb0d98af285dba4e1
Christian B <christianbear1221@gmail.com>
2020-06-02 00:27:07 -0700

    working on 'tree printing'

---
+++ .history/REDom_mountMe_20200602002650.js
@@ -0,0 +1,37 @@
+// pull imports
+const {el, mount} = redom;
+
+// class that takes in 'el', 'inner', and 'mountPoint'
+// then mounts said 'el' onto 'mountPoint'
+class MountMe{
+  constructor(){
+    this.innerMe=null;
+    this.mountPointMe=null;
+  }  
+
+  
+
+  make(elMe, inner, mountPoint){
+    var tmp = el(elMe, inner);
+    //console.log(mountPoint);
+    if(mountPoint==""||mountPoint==''){
+      mount(document.body, tmp);        // if left "" - then add to document.body
+    }else{
+       mount(document.querySelector(mountPoint), tmp);  
+    }
+
+    this.innerMe = inner;
+    this.mountPointMe = mountPoint;
+    
+  }
+  
+  printMe(idMe){
+    console.log(idMe);
+    if(this.mountPointMe == "" || this.mountPointMe==''){
+      console.log(document.body);
+    }else{
+      console.log(document.querySelector(this.mountPointMe));
+    }
+  }
+  
+}
